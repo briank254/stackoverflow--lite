c9818e72cfd4968cc0d0735d9b0625e529b692f4
graycadeau <g.zawadi.w@gmail.com>
2018-09-06 14:11:37 +0300

    [ft]question endpoints

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@
 import re
 from flask import request, jsonify
 
-from app.models import User
+from app.models import User, Question, QUESTIONS
 from . import app
 
 
@@ -51,3 +51,84 @@ def login_user():
         return jsonify({"msg": "Missing password parameter"}), 400
 
     return jsonify({"message": "Login successful"}), 201
+
+@app.route('/api/v1/questions/', methods=['POST'])
+def post_question():
+
+    """method to allow user to post a question"""
+
+    data = request.get_json()
+    question = data.get('question')
+
+    if question is not None and question.strip() == "":
+        return jsonify({
+            "message": "Please post a question"}), 400
+
+    question = Question(question=question)
+    question.add()
+    return jsonify({
+        "message": "Your query has been posted"}), 201
+
+
+@app.route('/api/v1/questions/', methods=['GET'])
+def get_all_questions():
+
+    """method to allow user to fetch all questions"""
+
+    query = Question()
+    questions = query.get_all()
+
+    return jsonify({
+        'questions': questions})
+
+
+@app.route('/api/v1/questions/<int:id>', methods=['GET'])
+def get_specific_question(id):
+
+    """method to allow user to fetch a specific question"""
+
+    query = Question()
+    question = query.get_specific(id)
+
+    if not question:
+        return jsonify({"message": "Query does not exist"}), 404
+
+    return jsonify({
+        'questions': question.serialize()})
+
+
+@app.route('/api/v1/questions/<int:id>', methods=['DELETE'])
+def delete_specific_question(id):
+
+    """method to allow user to delete a question"""
+
+    query = Question()
+    question = query.delete_specific(id)
+
+    if not question:
+        return jsonify({"message": "Query does not exist"}), 404
+
+    return jsonify({
+
+        "message": "Question deleted successfully"}), 200
+
+
+@app.route('/api/v1/questions/<int:id>', methods=['PUT'])
+def update_question(id):
+
+    """method to allow user to update a question"""
+
+    data = request.get_json()
+    query = data.get('question')
+    question = Question().get_specific(id)
+
+    if not question:
+        return jsonify({"message": "Query does not exist"}), 404
+
+    index = QUESTIONS.index(question)
+
+    if question:
+        QUESTIONS[index].question = query
+
+    return (jsonify({
+        "message": "Question updated successfully"}), 201)
