4c2020d708c7eb1280092ebfa1e8362e449a999c
graycadeau <g.zawadi.w@gmail.com>
2018-09-06 22:43:52 +0300

    initialize app as a module

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 """initializes app as a module"""
 
 from flask import Flask
-from . import routes
 
 app = Flask(__name__, instance_relative_config=True)
+
+from . import routes
 
 app.config.from_object('config')
