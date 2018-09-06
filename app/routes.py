f224b9239aab6608ab5dad0150449f0b86df0f06
graycadeau <g.zawadi.w@gmail.com>
2018-09-06 14:49:18 +0300

    [ft]answer endpoints

--- app/routes.py
+++ app/routes.py
@@ -3,13 +3,12 @@
 import re
 from flask import request, jsonify
 
-from app.models import User, Question, QUESTIONS
+from app.models import User, Question, Answer, QUESTIONS, ANSWERS
 from . import app
 
 
 @app.route('/api/v1/auth/signup', methods=['POST'])
 def signup_user():
-
     """method to signup user"""
 
     data = request.get_json()
@@ -34,7 +33,6 @@ def signup_user():
 
 @app.route('/api/v1/auth/signin', methods=['POST'])
 def login_user():
-
     """method to allow registered user to login"""
 
     if not request.is_json:
@@ -52,9 +50,9 @@ def login_user():
 
     return jsonify({"message": "Login successful"}), 201
 
+
 @app.route('/api/v1/questions/', methods=['POST'])
 def post_question():
-
     """method to allow user to post a question"""
 
     data = request.get_json()
@@ -72,7 +70,6 @@ def post_question():
 
 @app.route('/api/v1/questions/', methods=['GET'])
 def get_all_questions():
-
     """method to allow user to fetch all questions"""
 
     query = Question()
@@ -84,7 +81,6 @@ def get_all_questions():
 
 @app.route('/api/v1/questions/<int:id>', methods=['GET'])
 def get_specific_question(id):
-
     """method to allow user to fetch a specific question"""
 
     query = Question()
@@ -99,7 +95,6 @@ def get_specific_question(id):
 
 @app.route('/api/v1/questions/<int:id>', methods=['DELETE'])
 def delete_specific_question(id):
-
     """method to allow user to delete a question"""
 
     query = Question()
@@ -115,7 +110,6 @@ def delete_specific_question(id):
 
 @app.route('/api/v1/questions/<int:id>', methods=['PUT'])
 def update_question(id):
-
     """method to allow user to update a question"""
 
     data = request.get_json()
@@ -132,3 +126,103 @@ def update_question(id):
 
     return (jsonify({
         "message": "Question updated successfully"}), 201)
+
+
+@app.route('/api/v1/questions/<int:id>/answers', methods=['POST'])
+def post_answer(id):
+    """method to allow user to post an answer to a question"""
+
+    data = request.get_json()
+    question = Question().get_specific(id)
+    answer = data.get('answer')
+
+    if not question:
+        return jsonify({
+            "message": "Question unavailable"}), 400
+
+    if answer is not None and answer.strip() == "":
+        return jsonify({
+            "message": "Please post an answer"}), 400
+
+    answer = Answer(question=question, answer=answer)
+    answer.add()
+    return jsonify({
+
+        "message": "Your answer has been posted",
+        "answer": answer.serialize()
+
+    }), 201
+
+
+@app.route('/api/v1/questions/<int:id>/answers', methods=['GET'])
+def get_answers(id):
+    """method to allow user to fetch answers to a question"""
+
+    question = Question().get_specific(id)
+    answer = []
+
+    reply = Answer(question=question, answer=answer)
+    answers = reply.get_all_ans(id)
+
+    return jsonify({'answers': answers})
+
+
+@app.route('/api/v1/questions/<int:que_id>/answers/<int:ans_id>', methods=['GET'])
+def get_ans(que_id, ans_id):
+    """method to allow user to fetch an answer to a question"""
+
+    question = Question().get_specific(id)
+    answer = []
+
+    ans = Answer(question=question, answer=answer)
+    answer = ans.get_one(que_id, ans_id)
+
+    if not answer:
+        return jsonify({
+            "message": "Answer does not exist"}), 404
+
+    return jsonify({'answer': answer.serialize()})
+
+
+@app.route('/api/v1/questions/<int:que_id>/answers/<int:ans_id>', methods=['DELETE'])
+def delete_ans(que_id, ans_id):
+    """method to allow user to delete an answer to a question"""
+
+    question = Question().get_specific(id)
+    answer = []
+
+    ans = Answer(question=question, answer=answer)
+    answer = ans.delete_specific_answer(que_id, ans_id)
+
+    if not answer:
+        return jsonify({
+            "message": "Answer does not exist"}), 404
+
+    return jsonify({
+        "message": "Delete Successful"}), 200
+
+
+@app.route('/api/v1/questions/<int:que_id>/answers/<int:ans_id>', methods=['PUT'])
+def update_answer(que_id, ans_id):
+    """method to allow user to update an answer to a question"""
+
+    data = request.get_json()
+    ans = data.get('answer')
+    question = Question().get_specific(id)
+
+    answer = []
+    answer = Answer(question=question, answer=answer).get_one(que_id, ans_id)
+
+    if not answer:
+        return jsonify({
+            "message": "Answer does not exist"}), 404
+
+    index = ANSWERS.index(answer)
+
+    if answer:
+        ANSWERS[index].answer = ans
+
+    return jsonify({
+        "message": "Answer updated successfully",
+        "answer": answer.serialize()
+    }), 200
