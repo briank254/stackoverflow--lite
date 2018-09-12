6c9077afcd5dfe6fa71d437b0eb55c476c84e388
graycadeau <g.zawadi.w@gmail.com>
2018-09-12 18:27:47 +0300

    [chore]set environment

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ v2 application entry point
 import os
 from app_v2 import create_app
 
-app = create_app(os.getenv('ENV'))
+app = create_app(os.getenv('FLASK_ENV'))
 
 if __name__ == '__main__':
     app.run()
