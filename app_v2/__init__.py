b24abef2759227cb8c048487b6ea21b5d26b2dd0
graycadeau <g.zawadi.w@gmail.com>
2018-09-12 18:20:29 +0300

    [chore]initialize v2 app

--- app_v2/__init__.py
+++ app_v2/__init__.py
@@ -6,6 +6,7 @@ from flask_restful import Resource, Api
 from flask_jwt_extended import JWTManager
 from config import CONFIG
 from database.tables import create_tables
+from .auth import Signup
 
 
 def create_app(config_name):
@@ -15,9 +16,14 @@ def create_app(config_name):
 
     app = Flask(__name__)
     app.config.from_object(CONFIG[config_name])
+    app.config.from_pyfile('config.py', silent=True)
 
+    api = Api(app)
     create_tables(app.config['DATABASE'])
 
     JWTManager(app)
+
+    api.add_resource(Signup, '/api/v2/auth/signup')
+    
 
     return app
