89116b8f0bc5f8861f0d3fa33089bcc3573bcb19
graycadeau <g.zawadi.w@gmail.com>
2018-09-14 00:52:04 +0300

    [chore]adjust to adhere constant naming guide

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@
 
 from flask import Flask
 
-app = Flask(__name__, instance_relative_config=True)
+APP = Flask(__name__, instance_relative_config=True)
 
 from . import routes
 
-app.config.from_object('config')
+APP.config.from_object('config')
