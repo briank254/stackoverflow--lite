ad00fa9cfbfa04ddb03d743250f0720af2ca2d3a
graycadeau <g.zawadi.w@gmail.com>
2018-09-14 00:49:20 +0300

    [chore]adjust to adhere constant naming guide

--- server.py
+++ server.py
@@ -4,8 +4,8 @@ v2 application entry point
 import os
 from app_v2 import create_app
 
-app = create_app(os.getenv('FLASK_ENV'))
+APP = create_app(os.getenv('FLASK_ENV'))
 
 if __name__ == '__main__':
-    app.run()
+    APP.run()
     
