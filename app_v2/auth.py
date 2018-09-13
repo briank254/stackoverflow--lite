f3a9ccd26da2b8de625966ae80df7f7a79619b49
graycadeau <g.zawadi.w@gmail.com>
2018-09-13 15:57:55 +0300

    [chore]update user views

--- app_v2/auth.py
+++ app_v2/auth.py
@@ -7,25 +7,76 @@ from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 from jsonschema import validate, ValidationError
 from database.models.user_model import Users
-from .schemas import SIGNUP_SCHEMA
+from .schemas import SIGNUP_SCHEMA, SIGNIN_SCHEMA
 
 EMAIL_REGEX = re.compile(r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?")
 
 class Signup(Resource):
     def post(self):
         data = request.json
-        validate(data, SIGNUP_SCHEMA) 
+        try:
+            validate(data, SIGNUP_SCHEMA) 
 
-        if not re.match(EMAIL_REGEX, data['email']):
-            return{"message":"Invalid Email"}
+            if data['first_name'] is not None and data['first_name'].strip() == "":
+                return ({"message" : "Please fill in all the fields"}), 400
 
-        new_user = Users(data['first_name'],
-                         data['last_name'],
-                         data['email'],
-                         data['password']
-                        )
-        response = new_user.signup()
+            if data['last_name'] is not None and data['last_name'].strip() == "":
+                return ({"message" : "Please fill in all the fields"}), 400
 
-        return response 
+            if not re.match(EMAIL_REGEX, data['email']):
+                return{"message":"Invalid Email"}, 400
 
+            if data['password'] != data['confirm_password']:
+                return {'message': 'Passwords do not match'}, 400
+
+            new_user = Users(data['first_name'],
+                                data['last_name'],
+                                data['email'],
+                                data['password']
+                            )
+            response = new_user.signup()
+
+            return response 
+        except ValidationError as error:
+            return {'message': str(error)}, 400
+
+class Signin(Resource):
+    """
+    Login route handler
+    """
+    def post(self):
+       
+        data = request.json
+        try:
+            validate(data, SIGNIN_SCHEMA)
+
+            if not data['email']:
+                return ({"msg": "Missing email parameter"}), 400
+            if not data['password']:
+                return ({"msg": "Missing password parameter"}), 400
+        
+            email = data['email']
+            password = data['password']
+
+            response = Users.signin(email, password)
+
+            return response
+        except ValidationError as error:
+            return {'error': str(error)}, 400
+
+class User(Resource):
+    """
+    User resource routes
+    """
+    @jwt_required
+    def get(self):
+        
+        return Users.get_user_data()
+
+    @jwt_required
+    def put(self):
+        
+        data = request.json
+        
+        return Users.update_user(data)
 
