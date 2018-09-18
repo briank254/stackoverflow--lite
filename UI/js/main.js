708567cdfe71a1fc144d0b9683460b60bc2430df
graycadeau <g.zawadi.w@gmail.com>
2018-09-18 15:19:43 +0300

    [ft]fetch api

--- UI/js/main.js
+++ UI/js/main.js
@@ -18,24 +18,35 @@ function signup(event) {
 
     fetch("http:127.0.0.1:5000/api/v2/auth/signup", {
         method: "POST",
-        headers: {"Content-type": "application/json"},
+        headers: { "Content-type": "application/json" },
         body: JSON.stringify(data)
     })
-    .then((res) => {
-        if (res.status == 201) {
-            res.json().then((data) => {
-                window.location = "index.html";
-            });
-        }
-        else {
-            res.json().then((data) => {
-                alert.classList.toggle("show");
-                document.getElementById("message").textContent = data.message;
-            });
-        }
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+        .then((res) => {
+            return res.json()
+    
+        })
+        .then((data) => {
+            if(data.message === "Please fill in all fields"){
+                displayAlert("Please fill in all fields");
+            }
+            if(data.message === "Invalid Email"){
+                displayAlert("Input email in the format example@example.com");
+            }
+            if(data.message === "Password too short"){
+                displayAlert("Password should have more than 8 characters");
+            }
+            if(data.message === "Passwords do not match"){
+                displayAlert("Passwords do not match");
+            }
+            
+            if(data.message === "user already exists"){
+                displayAlert("user already exists");
+            }
+            if(data.message === "Account created"){
+                window.location.replace('index.html')
+            }
+        })
+        .catch(error => console.log(error))
+        
 }
 
